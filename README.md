ff4a96bdc1c9d8c21fb1cb469a5b1bc0bca0697a
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-12-04 17:41:28 +0100

    Update Christmas countdown functionality

---
+++ README.md
@@ -0,0 +1,59 @@
+<div align='center'>
+
+<br>
+
+# **christmas-countdown**
+
+**Can't wait for Christmas?**
+
+<br>
+
+</div>
+
+### Description
+
+Job or school is stressing you out? Can't wait for Christmas? This package is for you! It will tell you how much time is left to Christmas, so you can start counting down the days! 🎄
+
+### Installation
+
+Using `npm`:
+
+```sh
+$ npm i -g christmas-countdown
+```
+
+Using `yarn`:
+
+```sh
+$ yarn global add christmas-countdown
+```
+
+Using `pnpm`:
+
+```sh
+$ pnpm add -g christmas-countdown
+```
+
+Using `bun`:
+
+```sh
+$ bun add -g christmas-countdown
+```
+
+### Usage
+
+To check the version of the package, or just to see if it's installed correctly, run:
+
+```sh
+$ christmas-countdown -v
+```
+
+To see how much time is left to Christmas, run:
+
+```sh
+$ christmas-countdown
+```
+
+### License
+
+[MIT](https://github.com/alessandrofoglia07/nodejs-geolocation/blob/main/LICENSE)
--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,69 @@
 #! /usr/bin/env node
-console.log('Hello world!');
-export {};
+import chalk from 'chalk';
+import info from '../package.json' assert { type: 'json' };
+console.clear();
+if (process.argv[2] === '-v' || process.argv[2] === '--version') {
+    console.log(info.version);
+    process.exit(0);
+}
+const getTimeFromChristmas = () => {
+    const now = new Date();
+    let christmas = new Date(now.getFullYear(), 11, 25);
+    if (now > christmas) {
+        christmas = new Date(now.getFullYear() + 1, 11, 25);
+    }
+    const difference = christmas.getTime() - now.getTime();
+    const seconds = Math.floor(difference / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    return {
+        seconds: seconds % 60,
+        minutes: minutes % 60,
+        hours: hours % 24,
+        days,
+        date: christmas
+    };
+};
+const format = (num) => {
+    return num < 10 ? `0${num}` : num.toString();
+};
+const christmasText = (text, count) => {
+    let result = '';
+    text.split('').forEach((char, i) => {
+        if (count % 2 === 0) {
+            if (i % 2 === 0) {
+                result += chalk.red(char);
+            }
+            else {
+                result += chalk.white(char);
+            }
+        }
+        else {
+            if (i % 2 === 0) {
+                result += chalk.white(char);
+            }
+            else {
+                result += chalk.red(char);
+            }
+        }
+    });
+    return chalk.bold(result);
+};
+let count = 0;
+const display = () => {
+    console.clear();
+    const time = getTimeFromChristmas();
+    console.log(christmasText(`Merry Christmas! \n`, count));
+    const timeUntilChristmas = () => {
+        if (time.date.getFullYear() === new Date().getFullYear() && time.date.getMonth() === new Date().getMonth() && time.date.getDate() === new Date().getDate()) {
+            return christmasText('✨ Today is Christmas! Happy holidays! ✨', count);
+        }
+        else {
+            return `${time.days}:${format(time.hours)}:${format(time.minutes)}:${format(time.seconds)}`;
+        }
+    };
+    console.log(chalk.white.bold(`Time until ${chalk.redBright('Christmas')}: ${timeUntilChristmas()} \n\n(by @alessandrofoglia07)`));
+    count++;
+};
+setInterval(display, 1000);
